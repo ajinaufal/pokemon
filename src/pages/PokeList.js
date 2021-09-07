9196d0350a784f91be6ed1f2478135c345c51710
Naufal Bahri <msn.bahri@gmail.com>
2021-09-07 22:28:47 +0700

    ADD: MAS BAGIR

--- src/pages/PokeList.js
+++ src/pages/PokeList.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_POKEMONS } from "../Query/ListPoke";
 import { Container } from "purgecss/node_modules/postcss";
+import axios from "axios";
 
 function Home() {
     const { loading, error, data } = useQuery(GET_POKEMONS, {
@@ -19,11 +20,14 @@ function Home() {
             <h1 class="text-5xl text-center mb-7">List Pokemon</h1>
             {data && (
                 <div class="grid grid-cols-6 gap-2">
+                    {console.log(data)}
+                    {objPokemon(data.pokemons.results[0].url)}
                     {data.pokemons.results.map(item => (
                         <div class="box-content h-56 w-40 mx-auto bg-gray-800">
                             {/* <div class="grid grid-cols-2"> */}
                             <h1 class="text-lg text-white"> {item.name} </h1>
                             <img src={item.image} alt={item.name} />
+                            
                             {/* </div> */}
                         </div>
                     ))}
@@ -31,6 +35,13 @@ function Home() {
             )}
         </div>
     );
+}
+
+function objPokemon(url) {
+    axios.get(url)
+    .then((res) => {
+        console.log(res.data)
+    })
 }
 
 // function objpokemon(url) {
